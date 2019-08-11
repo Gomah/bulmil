1aa7e68aa10c6d6846464509da7f0e0c64d74909
Gomah <hi@gomah.fr>
2019-08-11 12:33:43 +1000

    chore(config): stencil testing

--- stencil.config.ts
+++ stencil.config.ts
@@ -25,10 +25,40 @@ export const config: Config = {
       type: 'dist-hydrate-script',
       dir: 'dist/hydrate',
     },
-    { type: 'docs-readme' },
+    { type: 'docs-readme', strict: true },
     {
       type: 'www',
       serviceWorker: null, // disable service workers
     },
   ],
+  testing: {
+    allowableMismatchedPixels: 200,
+    pixelmatchThreshold: 0.05,
+    waitBeforeScreenshot: 20,
+    emulate: [
+      {
+        userAgent: 'iPhone',
+        viewport: {
+          width: 400,
+          height: 800,
+          deviceScaleFactor: 2,
+          isMobile: true,
+          hasTouch: true,
+          isLandscape: false,
+        },
+      },
+      {
+        userAgent: 'Android',
+        viewport: {
+          width: 400,
+          height: 800,
+          deviceScaleFactor: 2,
+          isMobile: true,
+          hasTouch: true,
+          isLandscape: false,
+        },
+      },
+    ],
+  },
+  preamble: 'Bulmil - MIT License',
 };
