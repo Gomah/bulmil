55ab1dd59f27b3c4830232a0bc209b38d514ab9b
Gomah <hi@gomah.fr>
2019-10-03 10:55:14 +1000

    fix(table): import table styles globally

--- src/core/elements/table/test/table.spec.tsx
+++ src/core/elements/table/test/table.spec.tsx
@@ -5,49 +5,13 @@ it('Should render properly', async (): Promise<void> => {
   const page = await newSpecPage({
     components: [Table],
     html: `
-      <b-table is-striped>
-        <thead>
-          <tr>
-            <th>Tr Head</th>
-          </tr>
-        </thead>
-
-        <tbody>
-          <tr>
-            <th>Tr Body</th>
-          </tr>
-        </tbody>
-
-        <tfoot>
-          <tr>
-            <th>Tr Foot</th>
-          </tr>
-        </tfoot>
-      </b-table>
+      <b-table is-striped></b-table>
     `,
   });
 
   expect(page.root).toEqualHtml(`
     <b-table is-striped>
-      <table class="table is-striped">
-        <thead>
-          <tr>
-            <th>Tr Head</th>
-          </tr>
-        </thead>
-
-        <tbody>
-          <tr>
-            <th>Tr Body</th>
-          </tr>
-        </tbody>
-
-        <tfoot>
-          <tr>
-            <th>Tr Foot</th>
-          </tr>
-        </tfoot>
-      </table>
+      <table class="table is-striped"></table>
     </b-table>
   `);
 });
