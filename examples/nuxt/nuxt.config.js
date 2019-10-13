5707215ae1fc3e6207aecd07619b041cedd096fb
Gomah <hi@gomah.fr>
2019-10-13 16:37:43 +1100

    chore(examples): cleanup nuxt example

--- examples/nuxt/nuxt.config.js
+++ examples/nuxt/nuxt.config.js
@@ -23,7 +23,7 @@ export default {
   /*
    ** Global CSS
    */
-  css: [],
+  css: ['../../css/bulmil.css'],
   /*
    ** Plugins to load before mounting the App
    */
@@ -35,7 +35,7 @@ export default {
   /*
    ** Nuxt.js modules
    */
-  modules: ['@nuxtjs/pwa'],
+  modules: [],
   /*
    ** Build configuration
    */
--- examples/nuxt/pages/index.vue
+++ examples/nuxt/pages/index.vue
@@ -7,14 +7,9 @@
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
 export default {
-  components: {
-    Logo
-  },
-
   data: () => ({
+    text: 'Click Me!',
     size: null,
     color: 'is-primary',
     colors: [
@@ -53,27 +48,5 @@ export default {
   justify-content: center;
   align-items: center;
   text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
 }
 </style>
