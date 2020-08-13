25f068c85c20ad14d0cbb789d69617aaab092b3d
Gomah <hi@gomah.fr>
2020-08-13 14:29:09 +1000

    chore(plugins): adjust configuration for stencil plugins

--- packages/bulmil-react/src/components.ts
+++ packages/bulmil-react/src/components.ts
@@ -3,7 +3,7 @@
 /* auto-generated react proxies */
 import { createReactComponent } from './react-component-lib';
 
-import { JSX } from 'bulmil';
+import type { JSX } from 'bulmil';
 
 
 
--- packages/bulmil-vue/src/components.ts
+++ packages/bulmil-vue/src/components.ts
@@ -4,7 +4,7 @@
 import Vue, { PropOptions } from 'vue';
 import { createCommonRender, createCommonMethod } from './vue-component-lib/utils';
 
-import { Components } from 'bulmil';
+import type { Components } from 'bulmil';
 
 
 
