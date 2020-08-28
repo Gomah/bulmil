7aca1a7b08a3bb678b22b24defba38b28aab2206
Gomah <hi@gomah.fr>
2020-08-28 17:04:44 +1000

    tests(bulmil): adjust divider test

--- packages/bulmil/src/core/layout/divider/test/divider.spec.tsx
+++ packages/bulmil/src/core/layout/divider/test/divider.spec.tsx
@@ -4,11 +4,10 @@ import { Divider } from '../divider';
 it('Should render properly', async (): Promise<void> => {
   const page = await newSpecPage({
     components: [Divider],
-    html: `<bm-divider content="OR">
-           </bm-divider>`,
+    html: `<bm-divider>OR</bm-divider>`,
   });
 
   expect(page.root).toEqualHtml(`
-    <bm-divider class="is-divider" content="OR" data-content="OR"></bm-divider>
+    <bm-divider class="divider">OR</bm-divider>
   `);
 });
