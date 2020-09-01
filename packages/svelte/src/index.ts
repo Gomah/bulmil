23cc6094fba38d5e883c9b5f4e664f0cb2760b5e
Gomah <hi@gomah.fr>
2020-09-01 18:14:54 +1000

    fix(svelte): defineCustomElements in plugin

--- packages/svelte/src/index.ts
+++ packages/svelte/src/index.ts
@@ -1,7 +1,11 @@
-import { defineCustomElements } from '@bulmil/core/dist/loader';
-
+export * from '@bulmil/core';
 export * from './proxies';
 
 if (typeof window !== 'undefined') {
-  defineCustomElements(window);
+  const script = document.createElement('script');
+  script.type = 'module';
+  script.defer = true;
+  script.src = 'https://cdn.jsdelivr.net/npm/@bulmil/core@latest/dist/bulmil/bulmil.esm.js';
+  const firstScriptTag = document.getElementsByTagName('script')[0];
+  firstScriptTag.parentNode!.insertBefore(script, firstScriptTag);
 }
