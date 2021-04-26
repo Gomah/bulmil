4ca53ab7f5078998b1f1380e67ba552b271ce6eb
Gomah <hi@gomah.fr>
2021-04-26 16:04:26 +1000

    chore(stylelint): ignore import-at position rule

--- packages/core/.stylelintrc.js
+++ packages/core/.stylelintrc.js
@@ -3,6 +3,7 @@ module.exports = {
   plugins: ['stylelint-scss', 'stylelint-a11y', 'stylelint-prettier'],
   rules: {
     'prettier/prettier': true,
+    'no-invalid-position-at-import-rule': null,
     'selector-type-no-unknown': [true, { ignoreTypes: [/^bm-/] }],
     'at-rule-no-unknown': null,
     'no-descending-specificity': null,
