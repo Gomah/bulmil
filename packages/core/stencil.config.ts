da9ed8d90fe6239f11ecf09803760c9a3586c679
Gomah <hi@gomah.fr>
2022-01-30 11:09:58 +1100

    chore: bump bulma-switch & adjust stencil config

--- packages/core/stencil.config.ts
+++ packages/core/stencil.config.ts
@@ -63,7 +63,7 @@ export const config: Config = {
 
     { type: 'dist' },
     {
-      type: 'dist-custom-elements-bundle',
+      type: 'dist-custom-elements',
     },
     {
       type: 'docs-vscode',
